02ca7d318f4a453e3588b5cfb3e9b9758d2e74db
Alperen Akman <alperenakman45@gmail.com>
2024-07-17 09:42:05 +0300

    improve:

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ print(len(sys.argv))
 if len(sys.argv) != 2:
     print("Kullanim: python harf_sayaci.py <alperen>")
     sys.exit(1)
-fonk2(sys.argv[1])
-if len(sys.argv) >= 2:
-    print("Birden fazla kelime girdiniz...")
+fonk2(sys.argv[1]) 
+
--- tasks2.py
+++ tasks2.py
@@ -2,7 +2,7 @@ import sys
 from collections import Counter
 
 def fonk2(word):
-
+    word = sys.argv[1].replace(" ", "").upper()
     harf_sayaci = Counter(word)
 
     print(f"Kelime: {word}")
