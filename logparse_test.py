fa72357ec5dbf4508f38bafdb053c04755852014
Alperen Akman <alperenakman45@gmail.com>
2024-09-11 14:27:28 +0300

    Fix: logparse_test

--- logparse_test.py
+++ logparse_test.py
@@ -2,38 +2,37 @@ import unittest
 from unittest.mock import patch, mock_open
 from logparse import count_occurrences 
 
-class TestLogParse(unittest.TestCase):
+class LogParseTest(unittest.TestCase):
     def setUp(self):
         self.log_file_path = r'C:\Users\alper\OneDrive\Masaüstü\Alpi.log'
 
     @patch('builtins.open', new_callable=mock_open, read_data="2024-09-09 12:00:00 INFO: User logged in\n")
     def test_count_text_only(self, mock_file):
-        pattern = "User"
+        pattern = "User*"
         count = count_occurrences(pattern=pattern, file_path=self.log_file_path)
         self.assertEqual(count, 1)
 
     @patch('builtins.open', new_callable=mock_open, read_data="2024-09-09 12:00:00 INFO: User logged in\n2024-09-09 12:03:00 INFO: User logged out\n")
     def test_count_severity_only(self, mock_file):
-        severity = "INFO"
+        severity = "*INFO*"
         count = count_occurrences(severity=severity, file_path=self.log_file_path)
         self.assertEqual(count, 2)
 
     @patch('builtins.open', new_callable=mock_open, read_data="""2024-09-09 12:00:00 INFO: User logged in
-                                                                2024-09-09 12:01:00 ERROR: System failure
-                                                                2024-09-09 12:02:00 WARNING: Low memory
-                                                                2024-09-09 12:03:00 INFO: User logged out
-                                                                2024-09-09 12:04:00 INFO: Another user action
-                                                                """)
+2024-09-09 12:01:00 ERROR: System failure
+2024-09-09 12:02:00 WARNING: Low memory
+2024-09-09 12:03:00 INFO: System started""")
     
     def test_count_combined(self, mock_file):
-        pattern = "User"
-        severity = "INFO"
+        pattern = "*User*"
+        severity = "*INFO*"
         count = count_occurrences(pattern=pattern, severity=severity, file_path=self.log_file_path)
-        self.assertEqual(count, 2)
+        self.assertEqual(count, 1)
+
 
     @patch('builtins.open', new_callable=mock_open, read_data="2024-09-09 12:00:00 INFO: User logged in\n")
     def test_count_text_with_wildcard(self, mock_file):
-        pattern = "*User"
+        pattern = "*User*"
         count = count_occurrences(pattern=pattern, file_path=self.log_file_path)
         self.assertEqual(count, 1)
 
